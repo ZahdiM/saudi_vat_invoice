4a444f3872dbdfea4925c2866a58749a8fdb0156
zahidmehmood <zahid.mehmood@uhd.edu.iq>
2021-11-13 19:18:36 +0500

    added description

--- tax_reports/__manifest__.py
+++ tax_reports/__manifest__.py
@@ -1,9 +1,18 @@
 {
     # App information
-    'name': 'Tax Report',
+    'name': 'KSA VAT Invoice/Simplified VAT Reports/Standard and B2C VAT Invoices فاتورة ضريبیة',
     'version': '14.0.1.3',
     'category': 'Sales',
-    'summary': 'This Module will help in printing the Reports for Sales Order',
+    'summary': 'This Module will help in printing the Standard as well as B2C VAT Invoices',
+    'description': """ Simplified Tax Report
+    Simplified VAT Report
+    Saudi Arabia VAT Invoice
+    VAT E-Invoice Standard
+    E-Invoicing
+    B2C VAT Invoice
+    Fatora 
+    فاتورة ضريبیة
+    """,
     'license': 'OPL-1',
     'depends':['sale_management','base','web','account'],
 
@@ -25,10 +34,11 @@
         'reports/e_invoicing_b2c.xml',
         # 'view/inherit_web_external_layout.xml'
     ],
+    "images": ['static/description/Banner.png'],
     'demo_xml': [],
     'installable': True,
     'auto_install': False,
     'application': True,
-    'price': 379.00,
-    'currency': 'EUR',
+    'price': 30.00,
+    'currency': 'USD',
 }
